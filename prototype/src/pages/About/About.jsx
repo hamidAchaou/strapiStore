4758d19318354224dc7bd47b42a8685abaf8d662
Hamid Achaou <achaou.hamid.solicode@gmail.com>
2024-11-18 20:13:38 +0100

    save

--- prototype/src/pages/About/About.jsx
+++ prototype/src/pages/About/About.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import MainBanner from "./MainBanner";
 import AboutSection from "./AboutSection";
 import TeamSection from "./TeamSection";
 import ServiceSection from "./ServiceSection";
 import SubscribeSection from "./SubscribeSection";
 import "./About.css";
+import HeroSection from "../../components/HeroSection/HeroSection";
 
 const About = () => (
   <div>
-    <MainBanner />
+    {/* <MainBanner /> */}
+    <HeroSection
+      title={"About Our Company"}
+      details={"Awesome, clean &amp; creative HTML5 Template"}
+    />
     <AboutSection />
     <TeamSection />
     <ServiceSection />
